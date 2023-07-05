95e345ee74267d3b920c1111a21fa696598b07c9
Ali Brooks <36117420+abrookst@users.noreply.github.com>
2023-07-04 20:41:25 -0400

    Initial commit

---
+++ scripts/themetoggle.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    const themeValue = document.cookie.split("; ").find((row) => row.startsWith("theme="))?.split("=")[1];
+    if(themeValue == 'dark'){
+        document.cookie = "theme=dark;path=/";
+        document.documentElement.style.setProperty('--color4', '#d0d058');
+        document.documentElement.style.setProperty('--color3', '#a0a840');
+        document.documentElement.style.setProperty('--color2', '#708028');
+        document.documentElement.style.setProperty('--color1', '#405010');
+        document.getElementById('themeToggle').src  = './images/light.png';
+        document.getElementById('logo').src  = './images/logodark.png';
+    }
+    else{
+        document.cookie = "theme=light;path=/";
+        document.documentElement.style.setProperty('--color1', '#d0d058');
+        document.documentElement.style.setProperty('--color2', '#a0a840');
+        document.documentElement.style.setProperty('--color3', '#708028');
+        document.documentElement.style.setProperty('--color4', '#405010');
+        document.getElementById('themeToggle').src  = './images/dark.png';
+        document.getElementById('logo').src  = './images/logolight.png';
+    }
+
+    $('#themeToggle').click(function () {
+        const themeValue = document.cookie.split("; ").find((row) => row.startsWith("theme="))?.split("=")[1];
+        if(themeValue == 'dark'){
+            document.cookie = "theme=light;path=/";
+            document.documentElement.style.setProperty('--color1', '#d0d058');
+            document.documentElement.style.setProperty('--color2', '#a0a840');
+            document.documentElement.style.setProperty('--color3', '#708028');
+            document.documentElement.style.setProperty('--color4', '#405010');
+            document.getElementById('themeToggle').src  = './images/dark.png';
+            document.getElementById('logo').src  = './images/logolight.png';
+        }
+        else{
+            document.cookie = "theme=dark;path=/";
+            document.documentElement.style.setProperty('--color4', '#d0d058');
+            document.documentElement.style.setProperty('--color3', '#a0a840');
+            document.documentElement.style.setProperty('--color2', '#708028');
+            document.documentElement.style.setProperty('--color1', '#405010');
+            document.getElementById('themeToggle').src  = './images/light.png';
+            document.getElementById('logo').src  = './images/logodark.png';
+        }
+        console.log(document.cookie);
+        return false;
+    });
+});
