7c9cf8d020ef726ecb3930a83ddf3115a7c147dc
Ali Brooks <36117420+abrookst@users.noreply.github.com>
2024-09-11 01:43:55 -0400

    changing layout to be more like pictochat

--- scripts/projectSwitch.js
+++
@@ -1,7 +0,0 @@
-let active = null
-function projectSwitch(img){
-    active = document.getElementById("rightPortfolio")
-    let iframe = document.getElementById("rightPortfolio");
-    iframe.innerHTML = `<img src="`+img+`" style="max-height:100%; max-width:100%;">`
-
-}
